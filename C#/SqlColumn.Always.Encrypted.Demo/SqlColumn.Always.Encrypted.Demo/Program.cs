f9945642a6c10d210795ad6a8979527530381537
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-12-25 01:45:31 +0530

    Initial Version

---
+++ C#/SqlColumn.Always.Encrypted.Demo/SqlColumn.Always.Encrypted.Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlColumn.Always.Encrypted.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
