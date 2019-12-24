955351032df1f001eac7c637c39d095acaf23c1f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-12-25 01:51:27 +0530

    Added Console Application

---
+++ C#/SqlColumn.Always.Encrypted.Demo/Source/Collage.Application/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using static System.Console;
+
+namespace Collage.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            WriteLine("\n\nPress any key ...");
+            ReadKey();
+        }
+    }
+}
