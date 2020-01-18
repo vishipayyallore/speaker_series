f51f8dfa493acb2617e29ff0b6f9c889f8cd9f39
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-01-18 17:34:45 +0530

    Initial Version

---
+++ MicroServices/18Jan2020/College.Microservice/College.Microservice/Persistence/PersonDbContext.cs
@@ -0,0 +1,16 @@
+﻿using College.Microservice.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace College.Microservice.Persistence
+{
+    public class PersonDbContext : DbContext
+    {
+
+        public PersonDbContext(DbContextOptions<PersonDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<AddressData> AddressBook { get; set; }
+    }
+}
---
+++ MicroServices/18Jan2020/College.Microservice/CollegeService.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using System;
+using College.Microservice.Protos;
+using System.Threading.Tasks;
+
+namespace CollegeService.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new AddressBookService.AddressBookServiceClient(channel);
+
+            var userAddress = new AddressAdditionRequest { Name = "Manish", Address = "New Address" };
+
+            var reply = await client.AddUserAddressAsync(userAddress);
+
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
