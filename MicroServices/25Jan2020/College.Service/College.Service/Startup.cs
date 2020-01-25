59665aaeb0641f02569e61782fe9425ac64955fa
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-01-25 13:03:49 +0530

    Updates

--- MicroServices/25Jan2020/College.Service/College.Service/Startup.cs
+++ MicroServices/25Jan2020/College.Service/College.Service/Startup.cs
@@ -1,22 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using College.Microservice.Common;
+using College.Service.Persistence;
+using College.Service.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace College.Service
 {
     public class Startup
     {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+
+            // Adding EF Core
+            var connectionString = Configuration[Constants.ConnectionString];
+            services.AddDbContext<PersonDbContext>(o => o.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +45,7 @@ namespace College.Service
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<AddressBookSvc>();
 
                 endpoints.MapGet("/", async context =>
                 {
