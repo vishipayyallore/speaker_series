96eb3268ccab3b7c5e06507eef28da667d5f0101
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-02-01 22:04:43 +0530

    Initial Version

---
+++ MicroServices/01Feb2020/College.Service/College.ServiceClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using College.Service.Protos;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace College.ServiceClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new AddressBookService.AddressBookServiceClient(channel);
+
+            var userAddress = new AddressAdditionRequest { Name = "Manish", Address = "New Address" };
+
+            var reply = await client.AddUserAddressAsync(userAddress);
+
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MicroServices/01Feb2020/College.Service/CollegeService.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using College.Service.Protos;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace CollegeService.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new AddressBookService.AddressBookServiceClient(channel);
+
+            var userAddress = new AddressAdditionRequest { Name = "BDotNet", Address = "MSFT 25-Jan-2020" };
+
+            var reply = await client.AddUserAddressAsync(userAddress);
+
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
