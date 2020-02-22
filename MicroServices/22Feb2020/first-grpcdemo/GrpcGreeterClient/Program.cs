a455a70d873f1ae1023c7f16855188ea04a559b5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-02-22 18:38:12 +0530

    Basic Application

--- MicroServices/22Feb2020/first-grpcdemo/GrpcGreeterClient/Program.cs
+++ MicroServices/22Feb2020/first-grpcdemo/GrpcGreeterClient/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using Grpc.Net.Client;
+using GrpcGreater;
+using System.Threading.Tasks;
 
 namespace GrpcGreeterClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
