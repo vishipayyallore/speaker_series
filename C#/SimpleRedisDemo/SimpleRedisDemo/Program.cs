dd63632e476acda5fa5fc103fb130c7f5abccc20
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-03-06 14:54:02 +0530

    Initial Version

---
+++ C#/SimpleRedisDemo/SimpleRedisDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+
+namespace SimpleRedisDemo
+{
+    class Program
+    {
+        private static IConfigurationRoot Configuration { get; set; }
+        const string SecretName = "CacheConnection";
+
+        private static void InitializeConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+
+            Configuration = builder.Build();
+        }
+
+        private static readonly Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = Configuration[SecretName];
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            InitializeConfiguration();
+
+            IDatabase cache = lazyConnection.Value.GetDatabase();
+
+            // Simple PING command
+            string cacheCommand = "PING";
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            Console.WriteLine("Cache response : " + cache.Execute(cacheCommand).ToString());
+
+            // Simple get and put of integral data types into the cache
+            cacheCommand = "GET Shiva";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + cache.StringGet("Shiva").ToString());
+
+            Console.WriteLine("\n\nPress any key ...");
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ C#/SimpleRedisDemo/SimpleRedisDemo/ReadMe.txt
@@ -0,0 +1,4 @@
+﻿Install Microsoft.Extensions.Configuration.UserSecrets Nuget Package
+dotnet user-secrets set CacheConnection "Your Connection String to Redis"
+dotnet user-secrets --id "Your User Secret Id" set CacheConnection "Your Connection String to Redis" 
+
