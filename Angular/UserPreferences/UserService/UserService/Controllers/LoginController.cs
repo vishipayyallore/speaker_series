6f52e3cfe7a16ad1087689dc4f6185c6ef43b8e5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-03-12 23:05:45 +0530

    ..

---
+++ Angular/UserPreferences/UserService/UserService/Controllers/LoginController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using UserService.Entities;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors]
+    [Authorize]
+    [Produces("application/json")]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private List<Register> appUsers = new List<Register>();
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login([FromBody] Register model)
+        {
+            IActionResult response = Unauthorized();
+            Register user = AuthenticateUser(model);
+            if (user != null)
+            {
+                var tokenString = GenerateJWT(user);
+                response = Ok(new
+                {
+                    token = tokenString,
+                    userDetails = user,
+                });
+            }
+            return response;
+        }
+
+        string GenerateJWT(Register user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+              new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+              new Claim("Name", user.Name.ToString()),
+              new Claim("role",user.Role),
+              new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+          };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        Register AuthenticateUser(Register login)
+        {
+            Register user = appUsers.SingleOrDefault(x => x.Email == login.Email && x.Password == login.Password);
+            return user;
+        }
+
+    }
+}
---
+++ Angular/UserPreferences/UserService/UserService/Entities/Register.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Entities
+{
+    public class Register
+    {
+        public string Role { get; set; }
+        public string Name { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+
+}
