def9914646f35bc295dc2dcfcefa5f0e9e3c3a4a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-04-08 23:04:32 +0530

    Initial Version

---
+++ C#/RetrieveJwtTokeUsingRestSharp/RetrieveJwtTokeUsingRestSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace RetrieveJwtTokeUsingRestSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("http://localhost:50460");
+            var request = new RestRequest("/api/auth/token", Method.POST);
+
+            request.AddJsonBody(new
+            {
+                username = "userone",
+                password = "abcd123"
+            });
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content; // {"message":" created."}
+
+            UserAuthorization userAuthorization = JsonConvert.DeserializeObject<UserAuthorization>(content);
+            Console.WriteLine($"Token: {userAuthorization.Token} \nExpiration: {userAuthorization.Expiration}");
+
+            Console.WriteLine("\n\nPress any key ...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/RetrieveJwtTokeUsingRestSharp/RetrieveJwtTokeUsingRestSharp/UserAuthorization.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetrieveJwtTokeUsingRestSharp
+{
+    public class UserAuthorization
+    {
+        public string Token { get; set; }
+
+        public DateTime Expiration { get; set; }
+    }
+}
