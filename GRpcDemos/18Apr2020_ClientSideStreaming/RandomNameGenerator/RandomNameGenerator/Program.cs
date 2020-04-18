e98a0bbea2ca656587088bbbb1bd6d4628050811
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-04-18 16:29:38 +0530

    Initial Version

---
+++ GRpcDemos/18Apr2020_ClientSideStreaming/RandomNameGenerator/RandomNameGenerator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace RandomNameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var iCtr=0; iCtr <100; iCtr++)
+            {
+                Console.WriteLine($"Name: {GenerateName(10)}");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static string GenerateName(int len)
+        {
+            StringBuilder name = new StringBuilder(50);
+            Random random = new Random();
+            string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t", "v", "w", "x" };
+            string[] vowels = { "a", "e", "i", "o", "u", "ae", "y" };
+
+            
+            name.Append(consonants[random.Next(consonants.Length)].ToUpper());
+            name.Append(vowels[random.Next(vowels.Length)]);
+            int b = 2; 
+            while (b < len)
+            {
+                name.Append(consonants[random.Next(consonants.Length)]);
+                b++;
+                name.Append(vowels[random.Next(vowels.Length)]);
+                b++;
+            }
+
+            return name.ToString();
+        }
+
+    }
+}
