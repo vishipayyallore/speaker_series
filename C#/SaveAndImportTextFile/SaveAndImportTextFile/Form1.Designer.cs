4c3f78c5e71227584b40f98cb0e9188b30ae6f4e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-04-25 12:53:55 +0530

    Add Save to XML

--- C#/SaveAndImportTextFile/SaveAndImportTextFile/Form1.Designer.cs
+++ C#/SaveAndImportTextFile/SaveAndImportTextFile/Form1.Designer.cs
@@ -40,6 +40,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnImportFromXml = new System.Windows.Forms.Button();
+            this.btnSaveToXml = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.tubeDataBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,7 +49,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(84, 24);
+            this.label1.Location = new System.Drawing.Point(84, 16);
             this.label1.Name = "label1";
             this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label1.Size = new System.Drawing.Size(54, 17);
@@ -58,7 +60,7 @@
             // 
             this.btnSaveToText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSaveToText.ForeColor = System.Drawing.Color.Blue;
-            this.btnSaveToText.Location = new System.Drawing.Point(35, 132);
+            this.btnSaveToText.Location = new System.Drawing.Point(35, 114);
             this.btnSaveToText.Name = "btnSaveToText";
             this.btnSaveToText.Size = new System.Drawing.Size(137, 39);
             this.btnSaveToText.TabIndex = 1;
@@ -70,7 +72,7 @@
             // 
             this.btnImportFromText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnImportFromText.ForeColor = System.Drawing.Color.Blue;
-            this.btnImportFromText.Location = new System.Drawing.Point(181, 132);
+            this.btnImportFromText.Location = new System.Drawing.Point(181, 114);
             this.btnImportFromText.Name = "btnImportFromText";
             this.btnImportFromText.Size = new System.Drawing.Size(156, 39);
             this.btnImportFromText.TabIndex = 2;
@@ -82,7 +84,7 @@
             // 
             this.txtName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tubeDataBindingSource, "Name", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged, "No Name"));
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(148, 21);
+            this.txtName.Location = new System.Drawing.Point(148, 13);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(202, 23);
             this.txtName.TabIndex = 3;
@@ -96,7 +98,7 @@
             // 
             this.txtTubeOD.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tubeDataBindingSource, "TubeOD", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged, "0"));
             this.txtTubeOD.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTubeOD.Location = new System.Drawing.Point(148, 50);
+            this.txtTubeOD.Location = new System.Drawing.Point(148, 42);
             this.txtTubeOD.Name = "txtTubeOD";
             this.txtTubeOD.Size = new System.Drawing.Size(202, 23);
             this.txtTubeOD.TabIndex = 5;
@@ -106,7 +108,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(21, 52);
+            this.label2.Location = new System.Drawing.Point(21, 44);
             this.label2.Name = "label2";
             this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label2.Size = new System.Drawing.Size(120, 17);
@@ -117,7 +119,7 @@
             // 
             this.txtTubeWall.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tubeDataBindingSource, "TubeWall", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged, "0"));
             this.txtTubeWall.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTubeWall.Location = new System.Drawing.Point(148, 79);
+            this.txtTubeWall.Location = new System.Drawing.Point(148, 71);
             this.txtTubeWall.Name = "txtTubeWall";
             this.txtTubeWall.Size = new System.Drawing.Size(202, 23);
             this.txtTubeWall.TabIndex = 7;
@@ -127,7 +129,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(21, 81);
+            this.label3.Location = new System.Drawing.Point(21, 73);
             this.label3.Name = "label3";
             this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label3.Size = new System.Drawing.Size(128, 17);
@@ -142,11 +144,37 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // btnImportFromXml
+            // 
+            this.btnImportFromXml.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImportFromXml.ForeColor = System.Drawing.Color.Blue;
+            this.btnImportFromXml.Location = new System.Drawing.Point(181, 169);
+            this.btnImportFromXml.Name = "btnImportFromXml";
+            this.btnImportFromXml.Size = new System.Drawing.Size(156, 39);
+            this.btnImportFromXml.TabIndex = 9;
+            this.btnImportFromXml.Text = "Import from Xml";
+            this.btnImportFromXml.UseVisualStyleBackColor = true;
+            this.btnImportFromXml.Click += new System.EventHandler(this.btnImportFromXml_Click);
+            // 
+            // btnSaveToXml
+            // 
+            this.btnSaveToXml.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveToXml.ForeColor = System.Drawing.Color.Blue;
+            this.btnSaveToXml.Location = new System.Drawing.Point(35, 169);
+            this.btnSaveToXml.Name = "btnSaveToXml";
+            this.btnSaveToXml.Size = new System.Drawing.Size(137, 39);
+            this.btnSaveToXml.TabIndex = 8;
+            this.btnSaveToXml.Text = "Save To Xml";
+            this.btnSaveToXml.UseVisualStyleBackColor = true;
+            this.btnSaveToXml.Click += new System.EventHandler(this.btnSaveToXml_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(377, 192);
+            this.ClientSize = new System.Drawing.Size(377, 227);
+            this.Controls.Add(this.btnImportFromXml);
+            this.Controls.Add(this.btnSaveToXml);
             this.Controls.Add(this.txtTubeWall);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtTubeOD);
@@ -176,6 +204,8 @@
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.BindingSource tubeDataBindingSource;
+        private System.Windows.Forms.Button btnImportFromXml;
+        private System.Windows.Forms.Button btnSaveToXml;
     }
 }
 
--- C#/SaveAndImportTextFile/SaveAndImportTextFile/Form1.cs
+++ C#/SaveAndImportTextFile/SaveAndImportTextFile/Form1.cs
@@ -1,33 +1,56 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.ComponentModel;
 using System.IO;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace SaveAndImportTextFile
 {
+
     // Use this for saving files into XML https://www.newtonsoft.com/json/help/html/ConvertJsonToXml.htm
     public partial class Form1 : Form
     {
-        public TubeData _tubeData;
+        private TubeData _tubeData;
+        const string textFilePattern = "Text files (*.txt)|*.txt";
+        const string xmlFilePattern = "Xml files (*.xml)|*.xml";
 
         public Form1()
         {
             InitializeComponent();
             _tubeData = new TubeData
             {
-                Name= "No Name", TubeWall = 1, TubeOD = 1
+                Name = "No Name",
+                TubeWall = 1,
+                TubeOD = 1
             };
             tubeDataBindingSource.DataSource = _tubeData;
         }
 
-        private void btnSaveToText_Click(object sender, EventArgs e)
+        private void ShowFileSaveDialog(string filePattern, string title = "Save Tube Data as")
         {
-            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.Title = title;
+            saveFileDialog1.Filter = filePattern;
+            saveFileDialog1.FilterIndex = 1;
             saveFileDialog1.RestoreDirectory = true;
 
             saveFileDialog1.ShowDialog();
+        }
+
+        private void btnSaveToText_Click(object sender, EventArgs e)
+        {
+            const string title = "Save Tube Data as Text File";
+
+            ShowFileSaveDialog(textFilePattern, title);
+        }
+
+        private void btnSaveToXml_Click(object sender, EventArgs e)
+        {
+            const string title = "Save Tube Data as Xml File";
+
+            ShowFileSaveDialog(xmlFilePattern, title);
         }
 
         private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
@@ -35,26 +58,69 @@ namespace SaveAndImportTextFile
             // Get file name.
             string name = saveFileDialog1.FileName;
 
+            var fileContent = JsonConvert.SerializeObject(_tubeData);
+
+            if (saveFileDialog1.Filter.Contains("*.xml"))
+            {
+                XNode node = JsonConvert.DeserializeXNode(fileContent, "Root");
+                fileContent = node.ToString();
+            }
+
             // Write to the file name selected.
-            File.WriteAllText(name, JsonConvert.SerializeObject(_tubeData));
+            File.WriteAllText(name, fileContent);
         }
 
-        private void btnImportFromText_Click(object sender, EventArgs e)
+        private string GetFileNameFromOpenDialog(string filePattern, string title = "Import Tube Data from")
         {
+            string fileName = string.Empty;
+
+            openFileDialog1.Filter = filePattern;
+            openFileDialog1.Title = title;
             DialogResult result = openFileDialog1.ShowDialog();
 
-            if (result == DialogResult.OK) // Test result.
+            if (result == DialogResult.OK) 
             {
                 try
                 {
-                    string text = File.ReadAllText(openFileDialog1.FileName);
-                    _tubeData = JsonConvert.DeserializeObject<TubeData>(text);
-                    tubeDataBindingSource.DataSource = _tubeData;
+                    fileName = openFileDialog1.FileName;
                 }
                 catch (IOException ioException)
                 {
                     MessageBox.Show(ioException.Message);
                 }
+            }
+
+            return fileName;
+        }
+
+        private void btnImportFromText_Click(object sender, EventArgs e)
+        {
+            const string title = "Import Tube Data from Text File";
+            string fileName = GetFileNameFromOpenDialog(textFilePattern, title);
+
+            if(!string.IsNullOrWhiteSpace(fileName))
+            {
+                string text = File.ReadAllText(fileName);
+                _tubeData = JsonConvert.DeserializeObject<TubeData>(text);
+                tubeDataBindingSource.DataSource = _tubeData;
+            }
+        }
+
+        private void btnImportFromXml_Click(object sender, EventArgs e)
+        {
+            const string title = "Import Tube Data from Xml File";
+            string fileName = GetFileNameFromOpenDialog(xmlFilePattern, title);
+
+            if (!string.IsNullOrWhiteSpace(fileName))
+            {
+                string fileContent = File.ReadAllText(fileName);
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(fileContent);
+                
+                fileContent = JsonConvert.SerializeXmlNode(doc);
+                JObject json = JObject.Parse(fileContent);
+                _tubeData = JsonConvert.DeserializeObject<TubeData>(json.First.First.ToString());
+                tubeDataBindingSource.DataSource = _tubeData;
             }
         }
     }
