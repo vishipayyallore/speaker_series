394aa1dfb7debab2e4164e7f6c396a8376db42e9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-06-04 21:44:23 +0530

    Update Program.cs

--- C#/FileWordCount/FileWordCount/Program.cs
+++ C#/FileWordCount/FileWordCount/Program.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using static System.Console;
 
 namespace FileWordCount
 {
-    
+
     class Program
     {
         static void Main(string[] args)
@@ -17,15 +16,22 @@ namespace FileWordCount
 
             var words = fileContent.Replace("\r\n"," ").Split(" ");
 
-            if(words.Length < 5)
+            if (words.Length < 5)
             {
                 WriteLine("\n\nFile Should have minimum 5 words!!!");
             }
+            else
+            {
+                var wordAndCounts = words
+                    .GroupBy(w => w)
+                    .Select(g => new { Word = g.Key, Count = g.Count() })
+                    .ToList();
 
-            var counts = words
-                .GroupBy(w => w)
-                .Select(g => new { Word = g.Key, Count = g.Count() })
-                .ToList();
+                foreach (var item in wordAndCounts)
+                {
+                    WriteLine($"'{item.Word}' === {item.Count} times");
+                }
+            }
 
             WriteLine("\n\nPress any key ...");
             ReadKey();
