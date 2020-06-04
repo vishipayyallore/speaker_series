76149b17c8c89337148fa5f09f4e15a2cc6bfdc3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-06-04 21:34:52 +0530

    Initial Version

---
+++ C#/FileWordCount/FileWordCount/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using static System.Console;
+
+namespace FileWordCount
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = ".\\Data\\Sample1.txt";
+
+            var fileContent = File.ReadAllText(filePath);
+            WriteLine($"File Content: {fileContent}");
+
+            var words = fileContent.Replace("\r\n"," ").Split(" ");
+
+            var counts = words
+                .GroupBy(w => w)
+                .Select(g => new { Word = g.Key, Count = g.Count() })
+                .ToList();
+
+            WriteLine("\n\nPress any key ...");
+            ReadKey();
+        }
+    }
+
+}
